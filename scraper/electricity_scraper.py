a13bef1d5aeac54f001568d4f1ac9abc94d4e5d3
wulikanhua778899 <SNZCSNZC@outlook.com>
2023-11-24 15:55:40 +0800

    更新日志系统

--- scraper/electricity_scraper.py
+++ scraper/electricity_scraper.py
@@ -1,41 +1,26 @@
 import logging
-import os
 import random
 import time
-from datetime import date
 
 import scraper.slider_image_process as sip
 
 from playwright.sync_api import sync_playwright
 
-ROOT_DIRECTORY = os.path.dirname("../")
-FILE_NAME = f'{date.today()}_log.txt'  # 将每天的日期加入到日志文件名中
-LOG_DIRECTORY = os.path.join(ROOT_DIRECTORY, 'logs')
-
-# 检查并创建 logs 文件夹
-if not os.path.exists(LOG_DIRECTORY):
-    os.makedirs(LOG_DIRECTORY)
-
-LOG_FILE_PATH = os.path.join(LOG_DIRECTORY, FILE_NAME)
-
-logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
-
 class ElectricityScraper:
     def __init__(self, config):
-        print("初始化 ElectricityScraper")
-        print("初始化 self.config")
+        logging.info("初始化 ElectricityScraper")
+        logging.info("初始化 self.config")
         self.config = config
-        print("self.config 初始化完成")
-        print("ElectricityScraper 初始化完成")
+        logging.info("self.config 初始化完成")
+        logging.info("ElectricityScraper 初始化完成")
 
     def fetch_data(self):
         with sync_playwright() as pw:
-            print("尝试启动浏览器")
+            logging.info("尝试启动浏览器")
             self.browser = pw.chromium.launch(
-                # executable_path='C:\\Users\\ZeLin\\AppData\\Local\\ms-playwright\\chromium-1084\\chrome-win\\chrome.exe',
+                executable_path='C:\\Users\\ZeLin\\AppData\\Local\\ms-playwright\\chromium-1084\\chrome-win\\chrome.exe',
                 headless=True)
-            print("浏览器启动完成")
+            logging.info("浏览器启动完成")
             self.context = self.browser.new_context(viewport={'width': 1920, 'height': 1080})
             self.page = self.context.new_page()
 
@@ -79,14 +64,14 @@ class ElectricityScraper:
                 while self.page.locator(".cff8").nth(0).get_by_text('元').inner_text(timeout=5000)[:-1] == "--":
                     time.sleep(0.5)
                 amount = float(self.page.locator(".cff8").nth(0).get_by_text('元').inner_text()[:-1])
-                print(f"今日电费：{amount}")
+                logging.info(f"今日电费：{amount}")
             except:
                 self.page.screenshot(path='debug.png', full_page=True)
 
                 self.page.close()
                 self.context.close()
                 self.browser.close()
-                raise TimeoutError("Authentication or network error")
+                raise TimeoutError("验证或网络错误")
 
             self.page.close()
             self.context.close()
@@ -102,14 +87,14 @@ class ElectricityScraper:
             if not sip.is_monochrome('slide.png'):
                 break
             time.sleep(0.5)
-            print("Waiting slide image ...")
+            logging.info("等待验证码图片加载...")
 
             timeout -= 1
             if timeout <= 0:
                 self.page.close()
                 self.context.close()
                 self.browser.close()
-                raise TimeoutError("Timeout in wait slide image")
+                raise TimeoutError("加载验证码图片失败")
 
     def move_slide(self, distance):
         """移动滑块"""
