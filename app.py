cb6be553a3e0deacfbc82f1c47f2ed148efb28c7
okatu-loli <CN_QianShi@hotmail.com>
2023-11-26 19:22:53 +0800

    实现 SQLite 数据库以更好地处理数据
    此提交解决了应用中缺少持久性数据管理系统的问题。以前，电力数据存储在txt文件中，如果发生文件事故，可能会导致条目丢失。现在，添加了一个 SQLite 数据库来存储电力数据，这使得应用程序能够更高效、更安全地读写数据。此外，如果数据库不存在，则创建数据库。requirements.txt 已使用所需的包进行更新。

--- app.py
+++ app.py
@@ -1,16 +1,60 @@
 import configparser
+import os
+import sqlite3
 import time
+from datetime import datetime
 from threading import Thread
 
 import schedule
-from flask import Flask
+from flask import Flask, jsonify
 
 from main import Main
 
 app = Flask(__name__)
 
 
+def get_db_connection():
+    return sqlite3.connect('electricity.db')
+
+
+def check_and_create_db():
+    db_path = './electricity.db'
+    if not os.path.isfile(db_path):
+        try:
+            with get_db_connection() as conn:
+                cursor = conn.cursor()
+                create_table_string = "CREATE TABLE electricity (time REAL, expense REAL)"
+                cursor.execute(create_table_string)
+                print('数据库和表创建成功.')
+        except Exception as e:
+            print('创建数据库时遇到错误: ', str(e))
+
+
 def run_main():
+    # 获取当前时间
+    current_time = time.time()
+    current_date = datetime.utcfromtimestamp(current_time).date()
+
+    # 尝试从数据库获取最新记录的日期和电费
+    try:
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT time, expense FROM electricity ORDER BY time DESC LIMIT 1")
+            last_record = cursor.fetchone()
+
+            if last_record is not None:
+                last_date = datetime.utcfromtimestamp(last_record[0]).date()
+
+                # 如果最新记录的日期与当前日期相同，则返回当日的电费
+                if last_date == current_date:
+                    main = Main()
+                    main.notify(float(last_record[1]))
+                    return last_record[1]  # 返回当日电费
+    except Exception as e:
+        print("Error occurred:", e)
+        return f"err: {e}"
+
+    # 如果日期不同，则执行 main 函数
     config = configparser.ConfigParser()
     config.read('config.ini', encoding='utf-8')
     retry_times = int(config.get('Retry', 'retry_times'))
@@ -18,23 +62,57 @@ def run_main():
 
     main = Main()
     amount = main.run(retry_times=retry_times, retry_interval=retry_interval)
-    with open('electricity.txt', 'w') as file:
-        file.write(str(amount))
+
+    # 将新的记录插入数据库
+    try:
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("INSERT INTO electricity (time, expense) VALUES (?, ?)", (current_time, amount))
+            conn.commit()
+    except Exception as e:
+        print("Error occurred:", e)
+
+    return amount
 
 
 @app.route('/')
 def index():
-    return '欢迎访问!'
+    response = {
+        "status": 200,
+        "message": "欢迎访问!"
+    }
+    return jsonify(response), 200
 
 
-@app.route('/electricity')
+@app.route('/electricity', methods=['GET'])
 def get_electricity():
     try:
-        with open('electricity.txt', 'r') as file:
-            amount = file.read()
-        return amount
+        with get_db_connection() as conn:
+            cursor = conn.cursor()
+            cursor.execute("SELECT time, expense FROM electricity ORDER BY time DESC LIMIT 1")
+            record = cursor.fetchone()
+            response = {
+                "status": 200,
+                "data": {"time": record[0], "expense": record[1]}
+            }
+            return jsonify(response), 200
     except Exception as e:
-        return {"error": str(e)}, 500
+        response = {
+            "status": 500,
+            "message": "An error occurred: " + str(e)
+        }
+        return jsonify(response), 500
+
+
+@app.route('/getelectricity', methods=['POST'])
+def call_run_main():
+    try:
+        amount = run_main()
+        response = {"status": "success", "message": f"成功获取电费：{amount}."}
+        return jsonify(response), 200
+    except Exception as e:
+        response = {"status": "failure", "message": "发生了一个错误，请在重试时间结束后再尝试，具体错误: " + str(e)}
+        return jsonify(response), 500
 
 
 def run_schedule():
@@ -44,17 +122,15 @@ def run_schedule():
 
 
 if __name__ == '__main__':
+    check_and_create_db()
     config = configparser.ConfigParser()
     config.read('config.ini', encoding='utf-8')
 
     _time = config.get('Notification', 'time')
 
-    # 设定每天定时执行run_main函数
     schedule.every().day.at(f"{_time}").do(run_main)
 
-    # 创建并启动线程，用于运行schedule
     t = Thread(target=run_schedule)
     t.start()
 
-    run_main()
     app.run(host='0.0.0.0', port=5000)
