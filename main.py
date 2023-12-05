d1565fe1b3942d9208bc57ac16c2a23dcfcd1f25
wulikanhua778899 <SNZCSNZC@outlook.com>
2023-12-06 01:12:42 +0800

    增加了debug时保存图片的逻辑，方便定位错误。允许自定义超时时间，适应网速不好的情况，并且每次连接超时后可以自动增加下次超时时间。检验验证码时不经过硬盘，降低硬盘损耗，提升速度。

--- main.py
+++ main.py
@@ -17,13 +17,16 @@ if not os.path.exists(LOG_DIRECTORY):
 
 LOG_FILE_PATH = os.path.join(LOG_DIRECTORY, FILE_NAME)
 
-logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
+if not __name__ == "__main__":
+    logging.basicConfig(filename=LOG_FILE_PATH, level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
 
 class Main:
     def __init__(self):
         self.config = ConfigManager("config.ini")
-        self.scraper = ElectricityScraper(self.config)
+        self.retry_times = int(self.config.get("Retry", "retry_times"))
+        self.retry_interval = int(self.config.get("Retry", "retry_interval"))
+        self.page_timeout_addition = int(self.config.get("Retry", "page_timeout_addition"))
+        self.slide_timeout_addition = int(self.config.get("Retry", "slide_timeout_addition"))
         self.notifiers = None
 
         # 创建 Notifier 实例列表
@@ -36,23 +39,34 @@ class Main:
         for notifier in self.notifiers:
             notifier.send(amount, message)
 
-    def run(self, retry_times=3, retry_interval=60):
-        for i in range(retry_times):
+    def run(self):
+        retry_pageErr = 0
+        retry_slideErr = 0
+
+        for i in range(self.retry_times):
             try:
-                amount = self.scraper.fetch_data()
+                scraper = ElectricityScraper(self.config,
+                                             ST_addition = retry_slideErr * self.slide_timeout_addition,
+                                             PT_addition = retry_pageErr * self.page_timeout_addition)
+                amount = scraper.fetch_data()
                 if self.notifiers:
                     self.notify(amount)
                 return amount
             except TimeoutError as err:
-                if i < retry_times - 1:
-                    logging.error(f'尝试 {i + 1}次 失败，错误：{err}。 等待 {retry_interval}秒后再次尝试。')
-                    time.sleep(retry_interval)
+                if err.__str__() == "页面加载超时":
+                    retry_pageErr += 1
+                if err.__str__() == "验证码加载超时":
+                    retry_slideErr += 1
+
+                if i < self.retry_times - 1:
+                    logging.error(f'尝试 {i + 1}次 失败，错误：{err}。 等待{self.retry_interval}秒后再次尝试。')
+                    time.sleep(self.retry_interval)
                 else:
                     logging.error(f'尝试 {i + 1}次 失败，错误: {err}。 无更多尝试。')
 
 
 if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,format='%(asctime)s:%(levelname)s:%(message)s')
+
     main_instance = Main()
-    retry_times = int(main_instance.config.get("Retry", "retry_times"))
-    retry_interval = int(main_instance.config.get("Retry", "retry_interval"))
-    print(main_instance.run(retry_times=retry_times, retry_interval=retry_interval))
+    print(main_instance.run())
